e11f62dec3c9d32f44546c2f63568866bd910dc2
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-08-15 21:46:09 -0400

    completes and updates ShowsIndex jsx component

--- src/components/shows/ShowsIndex.jsx
+++ src/components/shows/ShowsIndex.jsx
@@ -1,10 +1,27 @@
 import { Link } from "react-router-dom";
-
+import { useState, useEffect } from "react";
 import ErrorMessage from "../errors/ErrorMessage";
-
+import ShowListing from "../shows/ShowListing"
+import { getAllShows } from "../../api/fetch";
 import "./ShowsIndex.css";
 
 export default function ShowsIndex() {
+
+  const [loading, setLoadingError] = useState(false)
+  const [shows, setShows] = useState ([]);
+
+  useEffect(() => {
+    getAllShows()
+    .then((showsJson) => {
+      setShows(showsJson)
+      setLoadingError(false)
+    })
+    .catch((err) => {
+      setLoadingError(true);
+      console.error(err)
+    })
+  },[])
+
   return (
     <div>
       {false ? (
@@ -26,6 +43,9 @@ export default function ShowsIndex() {
             />
           </label>
           <section className="shows-index">
+          { shows.map((show) => {
+              return <ShowListing show = {show} key = {show.id}/>
+            })}
             {/* <!-- ShowListing components --> */}
           </section>
         </section>
