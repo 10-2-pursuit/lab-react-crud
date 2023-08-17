147a62d096bd557001436335ea4fed0277e50870
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-08-16 20:09:00 -0400

    completes moviesindex component

--- src/components/movies/MoviesIndex.jsx
+++ src/components/movies/MoviesIndex.jsx
@@ -1,18 +1,35 @@
 import { Link } from "react-router-dom";
-
+import { useState, useEffect } from "react";
 import ErrorMessage from "../errors/ErrorMessage";
-
+import MovieListing from "../movies/MovieListing"
+import { getAllMovies } from "../../api/fetch";
 import "./MoviesIndex.css";
 
+export default function ShowsIndex() {
+
+  const [loading, setLoadingError] = useState(false)
+  const [movies, setMovies] = useState ([]);
+
+  useEffect(() => {
+    getAllMovies()
+    .then((showsJson) => {
+      setMovies(showsJson)
+      setLoadingError(false)
+    })
+  
+    .catch((err) => {
+      setLoadingError(true);
+      console.error(err)
+    })
+  },[])
 
 
-export default function MoviesIndex() {
   return (
     <div>
       {false ? (
         <ErrorMessage />
-    ) : (
-      <section className="movies-index-wrapper">
+      ) : (
+        <section className="movies-index-wrapper">
           <h2>All Movies</h2>
           <button>
             <Link to="/movies/new">Add a new movie</Link>
@@ -20,15 +37,18 @@ export default function MoviesIndex() {
           <br />
           <label htmlFor="searchTitle">
             Search Movies:
-            <input 
-             type="text"
-             id="searchTitle"
-             />
+            <input
+              type="text"
+              id="searchTitle"
+            />
           </label>
-        <section className="movies-index">
+          <section className="movies-index">
+          { movies.map((movie) => {
+              return <ShowListing movie = {movie} key = {movie.id}/>
+            })}
+          </section>
         </section>
-      </section>
       )}
     </div>
-  )
+  );
 }
